86fd27a9f85696472e4427372c737cd4efc7aec9
parade621 <parade621@naver.com>
2023-02-09 01:26:28 +0900

    검색 버튼 활성화

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,4 @@
 
-{
-Kapp/src/main/java/com/parade621/moviesearch/ui/view/RecentSearchFragment.kt,b\6\b6b3f1488ff3f6a184efa915a97c12cecb9de590
 u
 Eapp/src/main/java/com/parade621/moviesearch/ui/view/MoviewFragment.kt,9\c\9c08f0ee9a0604e50bd37240321505b5445d4299
 P
--- app/src/main/java/com/parade621/moviesearch/ui/view/SearchFragment.kt
+++ app/src/main/java/com/parade621/moviesearch/ui/view/SearchFragment.kt
@@ -1,19 +1,15 @@
 package com.parade621.moviesearch.ui.view
 
 import android.os.Bundle
-import android.text.Editable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.parade621.moviesearch.databinding.FragmentSearchBinding
 import com.parade621.moviesearch.ui.adapter.MovieSearchAdapter
 import com.parade621.moviesearch.ui.viewmodel.MovieSearchViewModel
-import com.parade621.moviesearch.util.Constants.SEARCH_TIME_DELAY
 
 class SearchFragment : Fragment() {
 
@@ -34,9 +30,10 @@ class SearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            searchFragment = this@SearchFragment
+        }
         movieSearchViewModel = (activity as MainActivity).movieSearchViewModel
-        setupRecyclerView()
-        searchBooks()
 
         movieSearchViewModel.searchResult.observe(viewLifecycleOwner) { response ->
             val movies = response.items
@@ -61,23 +58,10 @@ class SearchFragment : Fragment() {
         }
     }
 
-    private fun searchBooks() {
-        var startTime = System.currentTimeMillis()
-        var endTime: Long
-
-        binding.etSearch.addTextChangedListener { text: Editable? ->
-            endTime = System.currentTimeMillis()
-            if (endTime - startTime >= SEARCH_TIME_DELAY) {
-                text?.let {
-                    val query = it.toString().trim()
-                    if (query.isNotEmpty()) {
-                        movieSearchViewModel.searchMovies(query)
-                        Log.d("see Log:", query.toString())
-                    }
-                }
-            }
-            startTime = endTime
-        }
+    fun searchBooks() {
+        val query = binding.etSearch.text.toString()
+        movieSearchViewModel.searchMovies(query)
+        setupRecyclerView()
     }
 
     override fun onDestroyView() {
